2fbab15749b6077a2fe930e3b3d21cc364314ce2
Sarah <tans9@uci.edu>
2025-03-24 11:57:33 -0700

    added notes module into app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,15 @@
 import "dotenv/config";
 import express from "express";
+import NoteModel from "./models/note";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello bye!!!!!!!");
+//add async for code to use await so other code can run while getting data
+app.get("/", async (req, res) => {
+    //after the noteModel finds data, it will assign it to notes
+    const notes = await NoteModel.find().exec();
+    //turns notes into a json 
+    res.status(200).json(notes);
 });
 
 export default app;
---
+++ backend/src/models/note.ts
@@ -0,0 +1,13 @@
+import {InferSchemaType, model, Schema} from "mongoose";
+
+const noteSchema = new Schema({
+    title: {type: String, required: true},
+    text: {type: String},
+}, {timestamps: true});
+//timestamps is outside because it does not require user input and can be auto added
+
+//new type for typescript
+type Note = InferSchemaType<typeof noteSchema>;
+
+//exports a model of the new type we made with name note 
+export default model<Note>("Note", noteSchema);
