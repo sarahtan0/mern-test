1d0d45de5a1c47cf5682fadc632db369d3398367
Sarah <tans9@uci.edu>
2025-03-25 18:35:13 -0700

    connected db to react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Note } from './models/note';
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(() => {
+    //make it its own func to make it async
+    async function loadNotes(){
+      try{
+        //configures a get request from db
+        const response = await fetch("/api/notes", {method: "GET"});
+        //turns response into json, this is asynch
+        const notes = await response.json();
+        setNotes(notes);
+
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+    }
+    loadNotes();
+  }, []);
+  //empty array only executes func when page loads - if you put any vars it updates when the var updates
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {JSON.stringify(notes)}
     </div>
   );
 }
