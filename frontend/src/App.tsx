1b308a3e603462d6f5849070b6e896c76d1f8529
Sarah <tans9@uci.edu>
2025-03-26 01:57:39 -0700

    implemented add note to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Note as NoteModel} from './models/note';
 import Note from './components/Note';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Button } from 'react-bootstrap';
 import styles from "./styles/NotesPage.module.css";
+import * as NotesApi from "./network/notes_api";
+import AddNoteDialog from './components/AddNoteDialog';
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
+
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
 
   useEffect(() => {
     //make it its own func to make it async
     async function loadNotes(){
       try{
-        //configures a get request from db
-        const response = await fetch("/api/notes", {method: "GET"});
-        //turns response into json, this is asynch
-        const notes = await response.json();
+        //configures a get request from db and turns into json file
+        const notes = await NotesApi.fetchNotes()
         setNotes(notes);
 
       } catch (error) {
@@ -30,6 +31,10 @@ function App() {
 
   return (
     <Container>
+      <Button
+        onClick={() => setShowAddNoteDialog(true)}>
+        Add new note
+      </Button>
       <Row xs={1} md={2} lg={3} className="g-4">
         {notes.map(note => (
           <Col key={note._id}>
@@ -37,6 +42,12 @@ function App() {
           </Col>
         ))}
       </Row>
+      {showAddNoteDialog &&
+        <AddNoteDialog
+          onDismiss={() => setShowAddNoteDialog(false)}
+          onNoteSaved={() => {}}
+        />
+      }
     </Container>
   );
 }
