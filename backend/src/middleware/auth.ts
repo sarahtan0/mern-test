91dfc87b58ec441933865e826fbdce91f08cbccd
Sarah <sarahitan8@gmail.com>
2025-06-20 18:49:15 -0700

    added types to auth and installed remaining dev dependencies

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,7 +1,7 @@
-import { RequestHandler } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import createHttpError from "http-errors";
 
-export const requiresAuth: RequestHandler = (req, res, next) => {
+export const requiresAuth: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
     if(req.session.userId){
         //continue to the next handler (getAuthenticatedUser in this case)
         next();
