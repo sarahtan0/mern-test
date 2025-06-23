7cdd99e9f136f1a0a62929364a93c4058ec5c3e9
Sarah <sarahitan8@gmail.com>
2025-06-22 18:08:56 -0700

    debugging the fetchData call for login

--- frontend/src/network/notes_api.ts
+++ frontend/src/network/notes_api.ts
@@ -3,6 +3,8 @@ import { User } from "../models/user";
 
 //move the api calls to a diff module
 async function fetchData(input: RequestInfo, init?: RequestInit) {
+    console.log(input)
+    console.log(init)
     const response = await fetch(input, init);
     if(response.ok) {
         return response;
